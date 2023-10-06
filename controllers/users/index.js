4daa068edcb1ef84afdbf8fa3534eeb2cce8eb24
Jennifer Vendrell <isc20350326@gmail.com>
2023-10-06 10:23:03 -0600

    modularizacion con clase

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -1,4 +1,6 @@
-const listUsers = (req, res) => {
+const{request,response} = require(`express`);
+
+const listUsers = (req = request, res = reponse) => {
     res.json({msg: 'Users'})
 }
 
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,13 @@ const listUsers = require ('../controllers/Users')
 
 router.get('/', (req, res) => listUsers(req, res))
 
-module.exports = router
+router.get(`/`,listUser);
+//router.post(`/`,listUsers);
+//router.put(`/`,listUsers);
+//router.patch(`/`,listUsers);
+//router.delete(`/`,listUsers);
+
+
+module.exports= router
 
 // http://localhost:3000/api/v1/users
