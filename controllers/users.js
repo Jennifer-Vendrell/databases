287a88d248628ecef73601e2259ec747dcde92b4
Jennifer Vendrell <isc20350326@gmail.com>
2023-10-13 10:08:32 -0600

    validacion de un nuevo Endpoint que lista un nuevo usuario

--- controllers/users.js
+++ controllers/users.js
@@ -35,16 +35,31 @@ const listUserByID = async (req = request, res = response) => {
     const {id} = req.params;
 let conn;
 
+if (isNaN(id)){
+    res.status(400).json({msg:`The ID ${id} is invalid`});
+    return;
+}
+
+
+
 try{
     conn = await pool.getConnection();
 
-const user = await conn.query(usersModel.getByID,[id],(err)=> {
+const [user] = await conn.query(usersModel.getByID,[id],(err)=> {
     if (err){   
 
        throw err;
     
     }
 }) 
+
+
+if (!user){
+res.status(404).json({msg:`user with ID ${id}not found`});
+ return;
+
+}
+
 res.json(user);
 
 } catch (error){
@@ -57,4 +72,32 @@ res.json(user);
  }
 }
 
-module.exports = {listUsers, listUserByID}
+const addUser = async (req = request, res = response)=>{
+    let conn;
+
+    try{
+        conn = await pool.getConnection();
+
+
+        //nuevo endpoint//
+
+        const userAdded = await conn.query(usersModel.addRow,[user],(err)=>{
+            if (err) throw err;
+    
+        })
+    
+
+        console.log(userAdded);
+        res.json(userAdded);
+    } catch (error){
+        console.log(error);
+        res.status(500).json(error);
+    }finally {
+        if (conn) conn.end();
+    }
+}
+
+module.exports = {listUsers, listUserByID, addUser}
+
+
+//routes   -   controllers    - models(DB)
